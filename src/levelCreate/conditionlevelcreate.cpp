da94c9f7562d97f173ecb2e3a027887cbb1b65ff
TrubArt <vkag03@mail.ru>
2025-04-20 20:33:00 +0300

    added highlight for sections

--- src/levelCreate/conditionlevelcreate.cpp
+++ src/levelCreate/conditionlevelcreate.cpp
@@ -6,9 +6,87 @@ ConditionLevelCreate::ConditionLevelCreate(QWidget* parent)
     , ui(new Ui::ConditionLevelCreate)
 {
     ui->setupUi(this);
+    ui->buttonNext->setFocus();
+    ui->stackedWidget->setCurrentIndex(0);
+
+    m_sections.reserve(ui->sectionsLayout->rowCount());
+    for (int i = 0; i < ui->sectionsLayout->rowCount(); ++i)
+    {
+        m_sections.push_back(static_cast<QLabel*>(ui->sectionsLayout->itemAtPosition(i, 0)->widget()));
+    }
+
+    connectInitialization();
+}
+
+void ConditionLevelCreate::connectInitialization()
+{
+    connect(ui->buttonCancel, &QPushButton::clicked, this, &ConditionLevelCreate::reject);
+    connect(ui->buttonBack, &QPushButton::clicked, this, &ConditionLevelCreate::backClicked);
+    connect(ui->buttonNext, &QPushButton::clicked, this, &ConditionLevelCreate::nextClicked);
 }
 
 ConditionLevelCreate::~ConditionLevelCreate()
 {
     delete ui;
+}
+
+void ConditionLevelCreate::backClicked()
+{
+    int oldIndex = ui->stackedWidget->currentIndex();
+    int newIndex = oldIndex - 1;
+    if (newIndex < 0)
+    {
+        return;
+    }
+    if (oldIndex == ui->stackedWidget->count() - 1)
+    {
+        ui->buttonNext->setText("Next");
+    }
+
+    if (newIndex == 0)
+    {
+        ui->buttonBack->setEnabled(false);
+    }
+
+    ui->stackedWidget->setCurrentIndex(newIndex);
+    setSectionsFont(newIndex, ButtonClicked::back);
+}
+
+void ConditionLevelCreate::nextClicked()
+{
+    int oldIndex = ui->stackedWidget->currentIndex();
+    int countPage = ui->stackedWidget->count();
+    if (oldIndex == countPage - 1)
+    {
+        QDialog::accept();
+        return;
+    }
+
+    int newIndex = oldIndex + 1;
+    if (newIndex < countPage)
+    {
+        if (newIndex == countPage - 1)
+        {
+            ui->buttonNext->setText("Finish");
+        }
+
+        ui->buttonBack->setEnabled(true);
+        ui->stackedWidget->setCurrentIndex(newIndex);
+        setSectionsFont(newIndex, ButtonClicked::next);
+    }
+}
+
+void ConditionLevelCreate::setSectionsFont(int newPageIndex, ButtonClicked button)
+{
+    QFont font = m_sections[0]->font();
+    if (button == ButtonClicked::back)
+    {
+        font.setWeight(QFont::Weight::Normal);
+        m_sections[newPageIndex + 1]->setFont(font);
+    }
+    else // ButtonClicked::next
+    {
+        font.setWeight(QFont::Weight::Bold);
+        m_sections[newPageIndex]->setFont(font);
+    }
 }
--- src/levelCreate/conditionlevelcreate.h
+++ src/levelCreate/conditionlevelcreate.h
@@ -2,6 +2,8 @@
 #define CONDITIONLEVELCREATE_H
 
 #include <QDialog>
+#include <QLabel>
+#include <QVector>
 
 namespace Ui
 {
@@ -11,13 +13,23 @@ class ConditionLevelCreate;
 class ConditionLevelCreate : public QDialog
 {
     Q_OBJECT
+private:
+    enum class ButtonClicked { back, next };
 
 public:
     explicit ConditionLevelCreate(QWidget* parent = nullptr);
     ~ConditionLevelCreate() override;
+    void setSectionsFont(int newPageIndex, ButtonClicked button);
+
+public slots:
+    void backClicked();
+    void nextClicked();
 
 private:
+    void connectInitialization();
+
     Ui::ConditionLevelCreate* ui;
+    QVector<QLabel*> m_sections;
 };
 
 #endif // CONDITIONLEVELCREATE_H
